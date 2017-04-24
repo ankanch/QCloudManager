94021b557f1eceabfa4acd1d82ea6c1a80ff320c
Kanch <kanchisme@gmail.com>
2017-04-24 19:47:58 +0800

    APIKey的导入导出功能已经完成 #3-- ver 2.0.1

--- app/src/main/java/com/akakanch/qcloudmanager2/MainActivity.java
+++ app/src/main/java/com/akakanch/qcloudmanager2/MainActivity.java
@@ -1,15 +1,21 @@
 package com.akakanch.qcloudmanager2;
 
+import android.*;
+import android.Manifest;
+import android.app.Activity;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -20,12 +26,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.PopupMenu;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity
@@ -109,10 +119,39 @@ public class MainActivity extends AppCompatActivity
         }else if(id == R.id.action_kanch){
             Toast.makeText(getApplicationContext(),"测试",Toast.LENGTH_LONG).show();
         }else if(id == R.id.action_export_setting){
-            exportAPIKey();
+            //检查权限
+            if( ContextCompat.checkSelfPermission(getApplicationContext(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+                Toast.makeText(this,"授权完毕。请再次尝试导出！",Toast.LENGTH_LONG).show();
+            }else {
+                exportAPIKey();
+            }
+        }else if(id == R.id.action_import_setting){
+            //检查权限
+            if( ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+                Toast.makeText(this,"授权完毕。请再次尝试导入！",Toast.LENGTH_LONG).show();
+            }else {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("*/*");
+                startActivityForResult(intent, 199);
+            }
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == 199 && resultCode == Activity.RESULT_OK) {
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                Log.i("IMPORT-FILE-URI=", "Uri: " + uri.getPath());
+                importAPIKey(uri.getPath().replace("/document/",""));
+            }
+        }
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -182,7 +221,7 @@ public class MainActivity extends AppCompatActivity
             if(!file.mkdirs()){
                 Log.v("error-creating-dir=", "error");
             }
-            outputStream = new FileOutputStream(file+"/apikeydata");
+            outputStream = new FileOutputStream(file+"/apikeydata_on_"+new String().valueOf(System.currentTimeMillis()/1000));
             String defaultkey = read("API_KEY");
             String defaulyketId = read("API_KEY_ID");
             if (defaultkey.equals("NULL") || defaulyketId.equals("NULL")) {
@@ -191,34 +230,40 @@ public class MainActivity extends AppCompatActivity
             }
             outputStream.write((defaulyketId+"<@>"+defaultkey).getBytes());
             outputStream.close();
+            Toast.makeText(getApplicationContext(),"导出成功，APIKey数据已经导出至：\r\n"+file.getPath(),Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(getApplicationContext(),"导出失败，请重试！",Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
     }
 
-    /*/从外部储存导入API数据
-    public boolean importAPIKey(){
-        FileInputStream inputStream;
+    //*/从外部储存导入API数据
+    public boolean importAPIKey(String filepath){
+        BufferedReader inputStream;
         try {
-            File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOCUMENTS), "QCloudManager");
-            inputStream = new FileInputStream(file);
-            String var = inputStream.read();
+            inputStream = new BufferedReader(new FileReader(filepath));
+            String var = ((String)inputStream.readLine()).replace("\r","").replace("\n","");
             inputStream.close();
+            Log.v("file-content=",var);
             String defaultkey = new String();
             String defaulyketId = new String();
             String[] x= var.split("<@>");
             defaultkey = x[1];
-            defaulyketId = x[2];
-            if (defaultkey.equals("NULL") || defaulyketId.equals("NULL")) {
-                //解析失败，则报错
-                return false;
-            }
+            defaulyketId = x[0];
             save("API_KEY",defaultkey);
             save("API_KEY_ID",defaulyketId);
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            IndexPage ip = new IndexPage();
+            fragmentTransaction.replace(R.id.content_main,ip);
+            fragmentTransaction.commit();
+            this.setTitle(R.string.str_ma_title_index);
+            Toast.makeText(this,"导入成功！",Toast.LENGTH_LONG).show();
         } catch (IOException e) {
             e.printStackTrace();
+            Toast.makeText(this,"解析失败！可能是文件内容格式不正确！",Toast.LENGTH_LONG).show();
             return false;
         }
         return true;
